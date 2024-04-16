47b7a87b1db928ff4798c5683b827b9c01158492
shokorev25 <63199119+shokorev25@users.noreply.github.com>
2024-04-16 10:41:40 +0800

    radius_test.py

--- test/radius_test.py
+++ test/radius_test.py
@@ -1,1 +1,13 @@
+import math
+import pytest
+from radius import calculate_circle_area
 
+def test_calculate_circle_area():
+    assert calculate_circle_area(0) == 0
+    assert calculate_circle_area(1) == math.pi
+    assert calculate_circle_area(2.5) == math.pi * (2.5 ** 2)
+    with pytest.raises(ValueError):
+        calculate_circle_area(-1)
+
+if __name__ == "__main__":
+    pytest.main()
