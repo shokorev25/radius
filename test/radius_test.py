7a38cf8b769982772658f9b34e92662ef9b50e19
shokorev25 <63199119+shokorev25@users.noreply.github.com>
2024-04-16 20:54:41 +0800

    Update radius_test.py

--- test/radius_test.py
+++ test/radius_test.py
@@ -1,13 +1,13 @@
 import math
 import pytest
-from my_project.radius import calculate_circle_area
+from main.radius import calculate_circle_area
 
 def test_calculate_circle_area():
     assert calculate_circle_area(0) == 0
     assert calculate_circle_area(1) == math.pi
     assert calculate_circle_area(2.5) == math.pi * (2.5 ** 2)
     with pytest.raises(ValueError):
-        calculate_circle_area(-1)  # Проверяем, что функция вызывает исключение при отрицательном радиусе
+        calculate_circle_area(-1)
 
 if __name__ == "__main__":
-    pytest.main(["--pyargs", "my_project"])
+    pytest.main(["-v"])  
