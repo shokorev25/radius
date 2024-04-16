d605699258fab0eebda9f225de1e43b55e2a0780
shokorev25 <63199119+shokorev25@users.noreply.github.com>
2024-04-16 21:12:19 +0800

    Update radius_test.py

--- test/radius_test.py
+++ test/radius_test.py
@@ -2,12 +2,15 @@ import math
 import pytest
 from main.radius import calculate_circle_area
 
-def test_calculate_circle_area():
+def test_calculate_circle_area_with_zero_radius():
     assert calculate_circle_area(0) == 0
+
+def test_calculate_circle_area_with_radius_one():
     assert calculate_circle_area(1) == math.pi
+
+def test_calculate_circle_area_with_radius_two_point_five():
     assert calculate_circle_area(2.5) == math.pi * (2.5 ** 2)
+
+def test_calculate_circle_area_with_negative_radius():
     with pytest.raises(ValueError):
         calculate_circle_area(-1)
-
-if __name__ == "__main__":
-    pytest.main(["-v"])  
